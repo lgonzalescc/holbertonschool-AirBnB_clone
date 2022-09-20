291a1562783555570a3df8e055b56b43e51ba4ae
Luis Gonzales <luisgonzacc@gmail.com>
2022-09-20 14:31:22 +0000

    models

---
+++ tests/models/base_model.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""
+Class
+    a) BaseModel.
+"""
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """
+    Class:
+        BaseModel that defines all common attributes/methods
+        for other classes.
+    Attributes:
+        id (str):
+            ID unique of object.
+        created_at (datetime):
+            Datetime when an instance is created.
+        updated_at (datetime):
+            Datetime when an instance is created, and it will be updated.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Construct (replace):
+                BaseModel object.
+        Args:
+            *args (tuple):
+                Non-keyword variable length argument list.
+            **kwargs (dict):
+                Keyword variable length of arguments.
+        """
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+
+    def __str__(self):
+        """
+        Method (replace):
+            Represents the class objects as a string in format.
+        Return:
+            String in  format '[<class name>] (<self.id>) <self.__dict__>'.
+        """
+        class_str = "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
+        return class_str
+
+    def save(self):
+        """
+        Method:
+            Update auditory date and save change.
+        """
+        # note!: According to review it should be datetime.utcnow().
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        Method:
+            Generate dictionary of the class.
+        Return:
+            A dictionary containing all keys/values of __dict__ the instance.
+        """
+        class_dict = self.__dict__.copy()
+        class_dict['__class__'] = self.__class__.__name__
+        class_dict['created_at'] = class_dict['created_at'].isoformat()
+        class_dict['updated_at'] = class_dict['updated_at'].isoformat()
+        return class_dict
---
+++ tests/models/engine/file_storage.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+"""
+Class
+    a) FileStorage.
+"""
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+class FileStorage:
+    """
+    Class:
+        FileStorage that serializes instances to a JSON file
+        and deserializes JSON file to instances
+    Attributes:
+        FileStorage.__file_path (str):
+            Path to the JSON file.
+        FileStorage.__objects (dict):
+            Store all objects by <class name>.id.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Method:
+            Get de dictionary __objects.
+        Return:
+            The dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Method:
+            Sets in __objects the obj with key <obj class name>.id.
+        Args:
+            obj (object):
+                Object of create dictionary
+        """
+        key = "{0}.{1}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Method:
+           Serializes __objects to the JSON file (path: __file_path)
+        """
+        # info!: [objs_dict] Dictionary contains all dict of the objects
+        objs_dict = self.__objects.copy()
+
+        # info!: Serializable objects and Save in Json file
+        for key, obj in self.__objects.items():
+            objs_dict[key] = obj.to_dict()
+        # note!: Use mode W is correct or A?
+        with open(self.__file_path, mode="w") as file_json:
+            file_json.write(json.dumps(objs_dict))
+
+    def reload(self):
+        """
+        Method:
+            Deserializes the JSON file to __objects
+            (only if the JSON file (__file_path) exists).
+        """
+        class_dict = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review
+        }
+        try:
+            with open(self.__file_path, mode="r") as file_json:
+                # info!: values contain of dictionary attributes of the object
+                for key, values in json.load(file_json).items():
+                    class_name = values["__class__"]
+                    self.new(class_dict[class_name](**values))
+        except Exception:
+            pass
